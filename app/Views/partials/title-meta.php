d705607446673dd68ec5f9d7304a5c8d1397bdd6
arizki.putra <arizki.putra@phiraka.com>
2024-04-16 17:07:07 +0700

    update meta tag

--- app/Views/partials/title-meta.php
+++ app/Views/partials/title-meta.php
@@ -1,7 +1,9 @@
 <meta charset="utf-8" />
-<title><?= ($title) ? $title : '' ?> | Velzon - Admin & Dashboard Template</title>
+<title><?= ($title) ? $title : '' ?> | Halaman Landing Pramuka</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-<meta content="Themesbrand" name="author" />
+<meta content="Halaman landing untuk kegiatan pramuka yang kreatif dan
+menarik, dengan konten seputar Dewan Kerja Nasional dan kegiatan Pramuka Penegak
+Pandega." name="description" />
+<meta content="Dewan Kerja Nasional Gerakan Pramuka" name="author" />
 <!-- App favicon -->
 <link rel="shortcut icon" href="/assets/images/favicon.ico">
