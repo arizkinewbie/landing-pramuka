ad40a40ca6a9b960b86e9fdadcf7b47905fd7f31
arizkinewbie <arizkijitsu@gmail.com>
2024-04-18 16:17:09 +0700

    add leaflet css

--- app/Views/partials/head-css.php
+++ app/Views/partials/head-css.php
@@ -8,3 +8,5 @@
 <link href="/assets/css/app.min.css" rel="stylesheet" type="text/css" />
 <!-- custom Css-->
 <link href="/assets/css/custom.min.css" rel="stylesheet" type="text/css" />
+<!-- Leaflet Css -->
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
